de43a44b2bf4d1b03802bb846b9eb7c0a4583d2d
Abdurashid <aj6243434@gmail.com>
2021-09-07 11:42:06 +0500

    finish tabs and slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,18 +5,22 @@ menuArrow.addEventListener("click", () => {
   menuDropdown.classList.toggle("aktiv-dropdown");
   menuArrow.classList.toggle("arrow");
 });
+
+//
 //=================================
 // slider
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 0,
   grabCursor: true,
+  observer: true,
+  observeParents: true,
   autoplay: {
     delay: 4500,
     disableOnInteraction: false,
   },
 
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-paginationx",
     clickable: true,
   },
   breakpoints: {
@@ -36,91 +40,38 @@ var swiper = new Swiper(".mySwiper", {
 });
 //===================================================
 
-// tabbed content
-// http://www.entheosweb.com/tutorials/css/tabs.asp
-$(".tab_content").hide();
-$(".tab_content:first").show();
-
-/* if in tab mode */
-$("ul.tabs li").click(function () {
-  $(".tab_content").hide();
-  var activeTab = $(this).attr("rel");
-  $("#" + activeTab).fadeIn();
-
-  $("ul.tabs li").removeClass("active");
-  $(this).addClass("active");
-
-  $(".tab_drawer_heading").removeClass("d_active");
-  $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
-});
-/* if in drawer mode */
-$(".tab_drawer_heading").click(function () {
-  $(".tab_content").hide();
-  var d_activeTab = $(this).attr("rel");
-  $("#" + d_activeTab).fadeIn();
-
-  $(".tab_drawer_heading").removeClass("d_active");
-  $(this).addClass("d_active");
-
-  $("ul.tabs li").removeClass("active");
-  $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
-});
-
-/* Extra class "tab_last" 
-	   to add border to right side
-	   of last tab */
-$("ul.tabs li").last().addClass("tab_last");
-//=====================================================================
-// intro tab
-
-$(".tab_contentx").hide();
-$(".tab_contentx:first").show();
-
-/* if in tab mode */
-$("ul.tabsx li").click(function () {
-  $(".tab_contentx").hide();
-  var activeTab = $(this).attr("rel");
-  $("#" + activeTab).fadeIn();
-
-  $("ul.tabsx li").removeClass("activex");
-  $(this).addClass("activex");
-
-  $(".tab_drawer_headingx").removeClass("d_activex");
-  $(".tab_drawer_headingx[rel^='" + activeTabx + "']").addClass("d_activex");
-});
-/* if in drawer mode */
-$(".tab_drawer_headingx").click(function () {
-  $(".tab_contentx").hide();
-  var d_activeTab = $(this).attr("rel");
-  $("#" + d_activeTab).fadeIn();
-
-  $(".tab_drawer_headingx").removeClass("d_activex");
-  $(this).addClass("d_activex");
-
-  $("ul.tabsx li").removeClass("activex");
-  $("ul.tabsx li[rel^='" + d_activeTab + "']").addClass("activex");
-});
 //=============================================================
 // swiper in tab
-new Swiper('.swiper-container2', {
-  loop: true,
-  slideToClickedSlide: true,
+var swiper = new Swiper(".mySwiperx", {
+  slidesPerView: 3,
+  spaceBetween: 10,
   grabCursor: true,
+  observer: true,
+  observeParents: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-
   breakpoints: {
-    640: {
+    575: {
+      items:1,
       slidesPerView: 1,
-      spaceBetween: 0,
+      spaceBetween: 5,
     },
     768: {
-      slidesPerView: 1,
+      slidesPerView: 2,
       spaceBetween: 10,
     },
     1024: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+    1200: {
+      items: 3,
       slidesPerView: 3,
       spaceBetween: 25,
     },
